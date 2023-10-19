f398c14e8ed7f349a0c97bf99e4b2e411de31ca3
CordlessGnu <ben.hoggenator@gmail.com>
2023-10-19 10:04:32 +0100

    Tried Some String interpolation and Math.floor()

--- jsPractice/jsPracticeSesh1.js
+++ jsPractice/jsPracticeSesh1.js
@@ -2,3 +2,14 @@ let newVariable = 'Playing around with typeof.';
 console.log(typeof newVariable);
 newVariable = 1;
 console.log(typeof newVariable);
+
+/*The temperature today in kelvin, will not be converted so is a const*/
+const kelvin  = 293;
+//celsuis is 273 less than kelvin so we used the kelvin variable
+var celsius = kelvin - 273;
+//Used formula for converting celsuis to fahrenheit
+var fahrenheit = celsius * (9/5) + 32;
+//Used Math.floor() to round number
+var fahrenheit = Math.floor(fahrenheit);
+//Used string interpolation to display the variables value within text
+console.log("The temperatre is " + fahrenheit+ " degrees Fahrenheit.")
